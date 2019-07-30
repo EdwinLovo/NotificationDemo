56b7568ce504f5229df553abd0b30eee0a6d2d5e
EdwinLovo <00120617@uca.edu.sv>
2019-07-30 16:43:28 -0600

    topics

--- app/src/main/java/com/example/notificationdemo/MainActivity.java
+++ app/src/main/java/com/example/notificationdemo/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+        FirebaseMessaging.getInstance().subscribeToTopic("all2");
     }
 }
